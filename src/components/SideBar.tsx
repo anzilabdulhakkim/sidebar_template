fefb81d4fb6ada46b87fcdd1379489c6e431d485
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-07-30 16:53:32 +0530

    modified file

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaHome, FaInfoCircle, FaPhone } from 'react-icons/fa';
 
 // Sidebar component with navigation links
 const Sidebar: React.FC = () => {
   return (
-    <div className="w-64 h-screen bg-gray-800 text-white">
+    <div className="w-16 md:w-1/4 lg:w-1/5 h-screen bg-black text-white rounded-tr-lg rounded-br-lg">
       <div className="p-4">
         <h1 className="text-xl md:text-2xl lg:text-3xl font-bold text-center">SIDEBAR</h1>
         <hr className='mt-5'/>
         <nav className="mt-9">
-          <Link to="/" className="block p-2 text-sm md:text-base lg:text-lg hover:bg-gray-700 hover:border-r-2 hover:border-l-2 text-center">HOME</Link>
-          <Link to="/about" className="block p-2 text-sm md:text-base lg:text-lg hover:bg-gray-700 hover:border-r-2 hover:border-l-2 text-center">ABOUT</Link>
-          <Link to="/contact" className="block p-2 text-sm md:text-base lg:text-lg hover:bg-gray-700 hover:border-r-2 hover:border-l-2 text-center">CONTACT</Link>
+          <Link to="/" className="flex items-center justify-center p-2 text-sm md:text-base lg:text-lg hover:bg-gray-700 hover:border-r-2 hover:border-l-2 rounded-lg">
+            <FaHome className="mr-4" /> HOME
+          </Link>
+          <Link to="/about" className="flex items-center justify-center p-2 text-sm md:text-base lg:text-lg hover:bg-gray-700 hover:border-r-2 hover:border-l-2 rounded-lg">
+            <FaInfoCircle className="mr-4" /> ABOUT
+          </Link>
+          <Link to="/contact" className="flex items-center justify-center p-2 text-sm md:text-base lg:text-lg hover:bg-gray-700 hover:border-r-2 hover-border-l-2 rounded-lg">
+            <FaPhone className="mr-4" /> CONTACT
+          </Link>
         </nav>
       </div>
     </div>
